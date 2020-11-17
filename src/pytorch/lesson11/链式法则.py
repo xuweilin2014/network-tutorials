f5abc5c32ba63bda96ac1550c61e5386a530c34e
xuweilin2014 <xuweilin2014@outlook.com>
2020-11-17 16:41:36 +0800

    链式法则

---
+++ src/pytorch/lesson11/链式法则.py
@@ -0,0 +1,20 @@
+import torch
+from torch import autograd
+
+# 简单链式法则验证
+# dy2 / dw1 = (dy2 / dy1) * (dy1 / dw1)
+x = torch.tensor(1.)
+w1 = torch.tensor(2., requires_grad=True)
+b1 = torch.tensor(1.)
+w2 = torch.tensor(2., requires_grad=True)
+b2 = torch.tensor(1.)
+
+y1 = x * w1 + b1
+y2 = y1 * w2 + b2
+
+dy2_dy1 = autograd.grad(y2, [y1], retain_graph=True)[0]
+dy1_dw1 = autograd.grad(y1, [w1], retain_graph=True)[0]
+print(dy2_dy1 * dy1_dw1)
+
+dy2_dw1 = autograd.grad(y2, [w1], retain_graph=True)[0]
+print(dy2_dw1)
